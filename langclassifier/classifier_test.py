df11d7be4052bb9d154f29ad646671b1b257f435
Ioannis Charitopoulos <icharitopoulos@gmail.com>
2014-07-10 23:50:00 +0300

    setup tests

--- langclassifier/classifier_test.py
+++ langclassifier/classifier_test.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+import unittest
+
+import classifier
+
+
+class ClassifierTests(unittest.TestCase):
+
+    def test_first(self):
+        self.assertTrue(False)
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
