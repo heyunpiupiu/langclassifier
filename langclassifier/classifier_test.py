93adccf258755ef0ffcd9617908ded6eede72b96
Ioannis Charitopoulos <icharitopoulos@gmail.com>
2014-07-13 11:01:02 +0300

    start setting tests

--- langclassifier/classifier_test.py
+++ langclassifier/classifier_test.py
@@ -6,7 +6,16 @@ import classifier
 
 class ClassifierTests(unittest.TestCase):
 
-    def test_first(self):
+    def test_loads_settings(self):
+        self.assertTrue(False)
+
+    def test_loads_language_lists(self):
+        self.assertTrue(False)
+
+    def test_accepts_lists_as_input(self):
+        self.assertTrue(False)
+
+    def test_returns_dictionary(self):
         self.assertTrue(False)
 
 
