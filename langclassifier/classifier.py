3c7bb7141c4b03b45e54c9186f25238f4086c24c
Ioannis Charitopoulos <icharitopoulos@gmail.com>
2014-07-13 13:00:23 +0300

    passes tests, apart from loading settings

--- langclassifier/classifier.py
+++ langclassifier/classifier.py
@@ -7,21 +7,55 @@ class classifier:
 
         Languages structure is specifically set NOT as a class variable, so we
         can have multiple classes loaded with different language settings.
+
+        To optimise matching, the intersection of sets is used.
     """
 
     config_file = 'config_classifier'  # configuration file
 
-    def __init__(self):
+    def __init__(self, languages_to_use=None):
         """ Initialises class.
 
             Needs to initialise:
                 languages structure used for matching.
-        """
-        languages = {}  # dictionary holding the words we define as "key" for each language
-        pass
 
-    def classify(string_to_classify=None):
-        """ Attempts to match
+            Args:
+                languages_to_use: a list containing dictionaries defining language/words pairs.
+        """
+        self.languages = []  # list holding the words we define as "key" for each language
+
+        # if class was called with languages_to_use param
+        if isinstance(languages_to_use, list):
+            self.set_language_sets(languages_to_use)
+
+    def set_language_sets(self, languages_to_set):
+        """ Sets class instance language/words pairs, later used for the classification process.
+
+            languages class instance variable is a list of dictionaries, each dictionary containing:
+                lang_name: name of language.
+                words: a frozenset derived from a list provided, containing strings.
+
+            Args:
+                languages_to_set: a list of dictionaries.
+        """
+        if isinstance(languages_to_set, list):  # if param is list
+            for lang in languages_to_set:  # for each element in list
+                if isinstance(lang, dict):  # if element is dict
+                    if 'lang_name'in lang and 'words' in lang:  # if dict contains required attributes
+                        word_set = frozenset(lang['words'])  # create frozenset of words
+                        l = {'lang_name': lang['lang_name'], 'words': word_set}
+                        self.languages.append(l)  # append to instance language object
+
+    def classify(self, string_to_classify=None):
+        """ Attempts to match words contained within passed string with
+            words defined in
+
+            Args:
+                string_to_classify: string to be classified.
+
+            Returns:
+                dictionary containing the passed string and the language name it was classified under.
+                If string is not classified under any language, "could not classify" is returned.
         """
         # structure of returned object
         answer = {'classified_string': '', 'language_detected': ''}
--- langclassifier/classifier_test.py
+++ langclassifier/classifier_test.py
@@ -9,25 +9,55 @@ class ClassifierTests(unittest.TestCase):
     def test_loads_settings(self):
         self.assertTrue(False)
 
-    def test_accepts_lists_as_input(self):
-        self.assertTrue(False)
-
-    def test_does_not_load_invalid_language_list_dicts(self):
-        self.assertTrue(False)
+    def test_accepts_list_as_language_input(self):
+        lang_list = [{'lang_name': 'test', 'words': ['ping', 'pong']}]
+        try:
+            cl = classifier.classifier(languages_to_use=lang_list)
+        except:
+            pass
+        self.assertTrue(isinstance(cl, classifier.classifier))
 
     def test_loads_language_lists_to_instance(self):
-        self.assertTrue(False)
+        lang_list = [{'lang_name': 'test', 'words': ['ping', 'pong']}]
+        cl = classifier.classifier(languages_to_use=lang_list)
+        expected_internal_lang_structure = []
+        for lang in lang_list:
+            language_name_set_pair = {
+                'lang_name': lang['lang_name'],
+                'words': frozenset(lang['words'])
+                }
+            expected_internal_lang_structure.append(language_name_set_pair)
+        self.assertTrue(cl.languages == expected_internal_lang_structure)
+
+    def test_does_not_load_invalid_language_list_dicts(self):
+        lang_list = [
+            {'lang_name': 'valid', 'words': ['ping', 'pong']},
+            {'lang_name': 'invalid1', 'not_words': ['ping', 'pong']},
+            {'not_lang_name': 'invalid2', 'words': ['ping', 'pong']}
+            ]
+
+        expected_internal_lang_structure = [
+            {'lang_name': 'valid', 'words': frozenset(['ping', 'pong'])}
+            ]
+
+        cl = classifier.classifier(languages_to_use=lang_list)
+        self.assertTrue(cl.languages == expected_internal_lang_structure)
 
     def test_classify_returns_dictionary(self):
-        self.assertTrue(False)
+        cl = classifier.classifier()
+        output = cl.classify()
+        self.assertTrue(isinstance(output, dict))
 
     def test_classify_returns_dictionary_containing_expected_elements(self):
-        self.assertTrue(False)
-
-    def test_classify_returns_empty_dict_for_none_input(self):
         cl = classifier.classifier()
         output = cl.classify()
         self.assertTrue('classified_string' in output and 'language_detected' in output)
+
+    def test_classify_returns_empty_dict_for_none_input(self):
+        expected_answer = {'classified_string': '', 'language_detected': ''}
+        cl = classifier.classifier()
+        output = cl.classify()
+        self.assertTrue(output == expected_answer)
 
 
 def main():
