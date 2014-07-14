9b11febd91e929e267d1946181b9cb72896f3985
JC <i.charitopoulos@intelen.com>
2014-07-14 11:28:08 +0300

    added test language file

---
+++ langclassifier/config_languages_test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+languages = [
+        {
+            'lang_name': 'english',
+            'words': ['quick', 'lazy', 'dog']
+        },
+        {
+            'lang_name': 'dutch',
+            'words': ['vos', 'luie', 'hond']
+        },
+        {
+            'lang_name': 'german',
+            'words': ['Fuchs', 'Hund']
+        }
+    ]
